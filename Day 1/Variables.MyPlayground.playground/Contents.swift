a82a2a2ddcc906d41b259fab8b7f35359df4618b
Anthony Beckford <abeckford532@gmail.com>
2023-03-01 22:27:21 -0500

    Add files via upload

---
+++ Day 1/Variables.MyPlayground.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var name = "Anthony"
+name = "Keeley"
+
+
+let character = "Adonis"
+
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Alice"
+print(playerName)
+
+playerName = "Guile"
+print(playerName)
+
+// Integers
+
+var age = 32
+
+let reallyBig = 100_000_000
+
+let number = 120
+print(number.hashValue.isMultiple(of: 3))
+
+// decimals
+
+let a = 1
+let b = 2.0
+let c = Double(a) + b
+
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+
+
